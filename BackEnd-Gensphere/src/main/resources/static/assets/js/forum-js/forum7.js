f837b1f5c1da12dcd815b8aa921607e7a6497f89
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-06-12 14:31:50 -0600

    cambio en forum7 js

--- BackEnd-Gensphere/src/main/resources/static/assets/js/forum-js/forum7.js
+++ BackEnd-Gensphere/src/main/resources/static/assets/js/forum-js/forum7.js
@@ -38,7 +38,7 @@ var stompClient = null;
 function connect() {
     username_connect = userName
     if(username_connect) {
-        var socket = new SockJS('https://testgensphere.up.railway.app/websocket');
+        var socket = new SockJS('wss://testgensphere.up.railway.app/websocket');
         //var socket = new SockJS('https://testgensphere.up.railway.app/websocket');
         stompClient = Stomp.over(socket);
 
